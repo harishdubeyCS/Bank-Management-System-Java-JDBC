f14c10380b83b6dac548f96ba98e1d1b141c4247
harishdubeyCS <harishdubeycs@gmail.com>
2026-08-02 18:59:44 +0530

    Initial Commit

--- BankManagementSystem/src/DBConnection.java
+++ BankManagementSystem/src/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
       private static final String URL = "jdbc:mysql://localhost:3306/bankdb";
       private static final String USERNAME = "root";
-      private static final String PASSWORD = "harishccb";
+      private static final String PASSWORD = "your_password";
 
       public static Connection getConnection() {
           try{
